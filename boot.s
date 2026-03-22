a1d28de19a6c1e65136811b9b6ef2b0e2df8c550
Arconix-Orsheart <captainalex011@gmail.com>
2026-03-22 20:37:33 +0200

    changes in boot.s

--- boot.s
+++ boot.s
@@ -80,3 +80,35 @@ _start:
     runtime support to work as well.
     */
 
+    /*
+    Enter the high-level kernel. The ABI requires the stack is 16-byte
+    aligned at the time of the call instruction (which afterwards pushes
+    the return pointer to size 4 bytes). The stack was originally 16-byte
+    aligned above and we've pushed a multiple of 16 bytes to the 
+    stack since (pushed 0 bytes so far), so the alignment has thus been  
+    preserved and the call is well defined.
+    */
+    call kernel_main
+
+    /*
+    If the system has nothing more to do, put the computer into an 
+    infinite loop. To do that:
+    1) Disable interrupts with cli (clear interrupt enable in eflags).
+       They are already disabled by the bootloader, so this is not needed.
+       Mind that you might later enable interrupts and return from 
+       kernel_main (which is sort of nonsensical to do).
+    2) Wait for the next interrupt to arrive with hlt (halt instruction).
+       Since they are disabled, this will lock up the computer.
+    3) Jump to the hlt instruction if it ever wakes up due to a
+       non-maskable interrupt occurring or due to systen management mode.   
+    */
+    cli
+1:  hlt
+    jmp 1b
+
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing
+*/
+.size _start, . - _start
+
