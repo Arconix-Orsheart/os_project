a28098a6a61afcf9d6299a8c3e94c7ae28348085
Arconix-Orsheart <captainalex011@gmail.com>
2026-03-23 03:48:32 +0200

    changes

--- kernel.c
+++ kernel.c
@@ -89,6 +89,12 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c)
 {
+    if (c == '\n')
+    {
+        terminal_row++;
+        terminal_column = 0;
+        return;
+    }
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH)
     {
@@ -115,5 +121,5 @@ void kernel_main(void)
     terminal_initialize();
 
     /* Write a string to the terminal */
-    terminal_writestring("Hello, kernel World!\n");
+    terminal_writestring("Hello, kernel World!\nHow are you doing today?\n");
 }
